56448b3b4f4403d508f5c7d1751e0a0a36c511d6
blanketsucks <boyc9327@gmail.com>
2022-09-12 20:48:44 +0200

    Update artist.py

--- aiospotify/artist.py
+++ aiospotify/artist.py
@@ -23,6 +23,9 @@ class Artist(PartialArtist):
         self.popularity: int = data['popularity']
 
     async def fetch_top_tracks(self, *, market: Optional[str] = None) -> List[Track]:
+        # TODO: Even if the docs say the `market` argument is optional, it still raises the error if it's not present.
+        # Maybe default this to a value for example 'US'.
+        # All the country codes can be viewed at: https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Decoding_table
         data = await self._http.get_artist_top_tracks(id=self.id, market=market)
         return [Track(track, self._http) for track in data['tracks']]
 
